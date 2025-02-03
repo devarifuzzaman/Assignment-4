9c0d28034ca273649c0faf10505ebd83f8ed05d6
MD Arifuzzaman <mdarifuzzamanshakil12@gmail.com>
2025-02-03 23:56:40 +0600

    remove pkg

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,12 +3,9 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import "./index.css"
 import "bootstrap/dist/css/bootstrap.min.css"
-import {Analytics} from "@vercel/analytics/react";
-
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-      <Analytics />
     <App />
   </StrictMode>,
 )
