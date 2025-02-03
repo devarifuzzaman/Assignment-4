607f3f488b44093c95c78abfb726f008674c0f2d
MD Arifuzzaman <mdarifuzzamanshakil12@gmail.com>
2025-02-04 00:17:26 +0600

    add pkg

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ import Service from "./pages/Service.jsx";
 import Contact from "./pages/Contact.jsx";
 import Blog from "./pages/Blog.jsx";
 import SingleBlogSec from "./components/blogPage/SingleBlogSec.jsx";
-
+import {Analytics} from "@vercel/analytics/react";
 // import Loader from "./components/Loader.jsx";
 
 
@@ -14,6 +14,7 @@ const App = () => {
     return (
 
         <BrowserRouter>
+            <Analytics />
             {/*<Loader/>*/}
          <Routes>
 
